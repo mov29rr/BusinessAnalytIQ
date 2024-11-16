6134e5e04369b05f097fd52a04a4b407a77af23b
Maxim <>
2024-11-16 23:33:08 +0000

    Added proper OOP for asset distributions and investment clouds

--- portfolio_tab.py
+++ portfolio_tab.py
@@ -1,18 +1,19 @@
 from random import sample
 
-import pulp
 import streamlit as st
 import pandas as pd
-from importlib_metadata import distribution
-
 from pulp import LpMaximize, LpProblem, LpVariable
-import math  # For rounding
 
 import matplotlib.pyplot as plt
-from numpy.ma.core import minimum
 
+class InvestmentCloud:
+    def __init__(self, asset, expected_return, minimum, maximum):
+        self.asset = asset
+        self.expected_return = expected_return
+        self.minimum = minimum
+        self.maximum = maximum
 
-class CalculatedAssetDist:
+class AssetDist:
     def __init__(self, name, proportion):
         self.name = name
         self.proportion = proportion
@@ -20,12 +21,18 @@ class CalculatedAssetDist:
 def render():
     st.header("Portfolio")
 
-    # TODO: Proper OOP
+    # TODO: DB connection
+    investment_clouds = [
+        InvestmentCloud("S&P", 0.15, 35, 5),
+        InvestmentCloud("Apple", 0.2, 40, 50),
+        InvestmentCloud("Microsoft", 0.3, 30, 50),
+        InvestmentCloud("Facebook", 0.1, 25, 60)
+    ]
     table = {
-        "Asset": ["S&P 500", "Apple", "Microsoft", "Facebook"],
-        "Expected return": [0.15, 0.2, 0.3, 0.1],
-        "Minimum investment": [35, 60, 30, 25],
-        "Maximum investment": [5, 55, 50, 60]
+        "Asset": [ investment_cloud.asset for investment_cloud in investment_clouds ],
+        "Expected return": [ investment_cloud.expected_return for investment_cloud in investment_clouds ],
+        "Minimum investment": [ investment_cloud.minimum for investment_cloud in investment_clouds ],
+        "Maximum investment": [ investment_cloud.maximum for investment_cloud in investment_clouds ]
     }
 
     df = pd.DataFrame(table)
@@ -33,39 +40,31 @@ def render():
     edited_df = st.data_editor(df, use_container_width=True)
 
     if st.button("Optimise portfolio"):
-        optimised_asset_dist = calculate_optimal_asset_dist(table)
+        optimised_asset_dist = calculate_optimal_asset_dist(investment_clouds)
         display_assets_pie(optimised_asset_dist)
 
-def calculate_optimal_asset_dist(table):
-    asset_names = table["Asset"]
-    expected_returns = table["Expected return"]
-    minimum_investments = table["Minimum investment"]
-    maximum_investments = table["Maximum investment"]
-
+def calculate_optimal_asset_dist(investment_clouds):
     model = LpProblem("Total expected return", LpMaximize)
 
-    dists = [ LpVariable(asset_name, lowBound=0) for asset_name in asset_names ]
-    model += sum(dists[i] * expected_returns[i] for i in range(len(asset_names)))
+    asset_dists = [LpVariable(investment_cloud.asset, lowBound=0) for investment_cloud in investment_clouds]
+    model += sum(asset_dists[i] * investment_clouds[i].expected_return for i in range(len(investment_clouds)))
 
-    for i in range(len(asset_names)):
-        minimum_investment = minimum_investments[i]
-        maximum_investment = maximum_investments[i]
+    for i in range(len(investment_clouds)):
+        model += investment_clouds[i].minimum <= asset_dists[i]
+        model += investment_clouds[i].maximum >= asset_dists[i]
 
-        model += minimum_investment <= dists[i]
-        model += maximum_investment >= dists[i]
-
-    model += sum(dist for dist in dists) == 100
+    model += sum(asset_dist for asset_dist in asset_dists) == 100
 
     model.solve()
 
-    return [ CalculatedAssetDist(dist.name, dist.value()) for dist in dists ]
+    return [ AssetDist(asset_dist.name, asset_dist.value()) for asset_dist in asset_dists ]
 
 def display_assets_pie(assets):
 
     labels = [ asset.name for asset in assets ]
     sizes = [ asset.proportion for asset in assets ]
     # TODO: Randomise color generation
-    colors = ["#ff9999","#66b3ff","#99ff99","#ffcc99"]
+    colors = ["#ff9999","#66b3ff","#99ee99","#ffcc99"]
 
     fig, ax = plt.subplots()
     ax.pie(sizes, labels=labels, colors=colors, autopct="%1.1f%%", startangle=90, textprops={"color": "white"})
