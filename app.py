98943d34f572e2aaeed5853f0558ea5a740541f0
Arungading <rhyswomack@gmail.com>
2024-11-17 10:17:36 +0000

    Added code to make table using DB data

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 from portfolio_analysis import InvestmentCloud, calculate_optimal_asset_dist, display_assets_pie, NonFeasibleSolutionError
+from pymongo import MongoClient
 from flask import Flask, render_template, Response
+
+#Database client url
+uri = "mongodb+srv://rwomack1325:esMBO1BvvqjzpReA@maindb.rluzm.mongodb.net/?retryWrites=true&w=majority&appName=MainDB"
+client = MongoClient(uri)
+
+#Database being searched
+database = client.get_database("AnalytIQDB")
+
+#Collection(s)/table(s) being searched
+assetsCollection = database.get_collection("Assets")
+
+#Raw Data from DB
+listofRawAssets = assetsCollection.distinct("asset")
+listofRawER = assetsCollection.distinct("expected return")
+listofRawMin = assetsCollection.distinct("minimum investment")
+listofRawMax = assetsCollection.distinct("maximum investment")
+
+#Sanitising the data, putting it in lists
+sanitisedAssets = [str(asset).strip("[]\"\'") for asset in listofRawAssets]
+sanitisedER = [float(er) for er in listofRawER]
+sanitisedMin = [float(min) for min in listofRawMin]
+sanitisedMax = [float(max) for max in listofRawMax]
+
+#Counting records
+record_count = min(len(sanitisedAssets), len(sanitisedER), len(sanitisedMin), len(sanitisedMax))
 
 app = Flask(__name__)
 
-investment_clouds = [
-    InvestmentCloud("S&P 500", 0.15, .10, .55),
-    InvestmentCloud("Apple", 0.2, .11, .50),
-    InvestmentCloud("Microsoft", 0.3, .05, .50),
-    InvestmentCloud("Facebook", 0.1, .50, .60)
-]
+#List of investments
+investment_clouds = []
+
+#Add assets from DB to list of investments
+for i in range(record_count):
+    investment_clouds.append(InvestmentCloud(sanitisedAssets[i], sanitisedER[i], sanitisedMin[i], sanitisedMax[i]))
+    i += 1
 
 @app.route("/asset_dist.png")
 def asset_dist():
